d9877f1287f3ea110b13d2346500b6f09fec497e
Michael Yaruta <ingellion@gmail.com>
2021-02-01 17:27:16 +0300

    5th commit

--- server.js
+++ server.js
@@ -1,64 +1,48 @@
+const mongoose = require('mongoose');
 const express = require('express');
-const path = require('path');
-const jwt = require('jsonwebtoken');
-const port = process.env.PORT || 8080;
-const User = require('../models/User');
+const config = require('config');
+const path = require('path')
 
-// здесь у нас происходит импорт пакетов и определяется порт нашего сервера
+
 const app = express();
 
-//здесь наше приложение отдаёт статику
 app.use(express.json({ extended: true }));
-
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 
 //простой тест сервера
 app.get('/ping', function (req, res) {
-  return res.json('pong');
+  return res.send('pong');
 });
 
-app.post(
-  '/createUser',
-  async (req, res) => {
-    try{
 
-      console.log('logintratatata')
-
-
-      // let oldUser = await User.findOne({Login: req.body.login});
-      //
-      // if(oldUser){
-      //   return res.status(400).json({ message: `This user already exists` });
-      // }
-
-      const user = new User ({
-        Login: req.body.login,
-        Password: req.body.password,
-        filter: 'all'
-      });
-
-
-
-      const token = jwt.sign(
-        { userId: user._id },
-        'mikeYarutische',
-        { expiresIn: '1h' }
-      );
-
-      //await user.save();
-
-      res.status(200).json({token, userId: user._id, userName: user.Login, filter: user.filter});
-
-    }catch (e) {
-
-      res.status(500).json({ message: e.message});
-
-    }
-  });
-
+app.use('/app/todoApp', require('./routes/task.routes'));
+app.use('/app/auth', require('./routes/auth.routes'));
 //обслуживание html
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen(port);
+
+const PORT = process.env.PORT || config.get('port');
+
+async function start(){
+  try{
+    await mongoose.connect(process.env.MONGODB_URI || config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+
+    console.log('mongo has been started');
+
+    app.listen(PORT, () => console.log(`app has been started on port ${PORT}...`));
+
+  }catch(e){
+
+    console.log('Server Error', e.message);
+
+    process.exit(1);
+  }
+}
+
+start();
