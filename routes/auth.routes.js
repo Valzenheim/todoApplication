40225db2230c04c025277a383065590ab41ce62e
Michael Yaruta <ingellion@gmail.com>
2021-02-02 12:37:39 +0300

    Added authentication middleware

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -54,9 +54,6 @@ router.post(
     ],
     async (req, res) => {
     try{
-
-        console.log('logintratatata')
-
         const errors = validationResult(req);
 
          if ( !errors.isEmpty() ){
